b8051b432f38fba871cf0738f2405d185495cb94
bob.nem <bob.nem@ya.ru>
2021-07-23 16:01:06 +0600

    Third block cards

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
 import style from '../styles/Home.module.css';
 import Image from 'next/image';
 import { ReactElement } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
 import topIMG from '../images/Top-IMG.png';
+import unicornTopLogo from '../images/unicorn-logo-gr.png';
 import firstBlockIMG from '../images/FirstBlock-IMG.png';
 import secondBlockIMG from '../images/SecondBlock-IMG.png';
 import windowTargetIcon from '../images/window-target1.svg';
 import manyProcessessIcon from '../images/many-processes.svg';
 import employeeStudying from '../images/employee-studying.svg';
-import unicornTopLogo from '../images/unicorn-logo-gr.png';
+import customerBase from '../images/customer-base.svg';
+import customerSupport from '../images/customer-support.svg';
+import employeesKPI from '../images/employees-kpi.svg';
+import telephony from '../images/telephony.svg';
+import systemIntegration from '../images/integration-systems.svg';
+import processesModeling from '../images/processes-modeling.svg';
+import checkWhite from '../images/check-white.svg'
 
 export default function Home() {
   return (
@@ -98,28 +103,28 @@ export default function Home() {
             <h2 className={style.SecondBlockHeader}>Stages of CRM development and implementation</h2>
             <div className={style.SecondBlockLine}></div>
             <div className={style.SecondBlockConent}>
-              <FontAwesomeIcon icon={faCheck} className={style.SecondBlockIcon}/>
+              <div className={style.SecondBlockIcon}><Image src={checkWhite} alt="check icon"/></div>
               <div>
                 <h3>Technical task development</h3>
                 <p>Analysis of business processes of the enterprise, development of a flowchart, a detailed description of the functionality. Selecting the optimal CRM solution for solving your tasks, determining the stages of implementation and the results of each stage.</p>
               </div>
             </div>
             <div className={style.SecondBlockConent}>
-              <FontAwesomeIcon icon={faCheck} className={style.SecondBlockIcon}/>
+              <div className={style.SecondBlockIcon}><Image src={checkWhite} alt="check icon"/></div>
               <div>
                 <h3>Interface design, programming</h3>
                 <p>Interface design development, module programming. Working in a CRM system has its own nuances and features, we will help to optimize the process, make it more understandable for the employee. The execution of all actions in the system will become obvious and intuitive. Using the example of specific tasks, we show how CRM will help simplify the work for him.</p>
               </div>
             </div>
             <div className={style.SecondBlockConent}>
-              <FontAwesomeIcon icon={faCheck} className={style.SecondBlockIcon}/>
+              <div className={style.SecondBlockIcon}><Image src={checkWhite} alt="check icon"/></div>
               <div>
                 <h3>Testing and launching</h3>
                 <p>Project testing, integration, warranty support, staff training. An employee who knows his product well, sales methods and tools that increase efficiency, which is CRM, has great value. Such an employee becomes a much more important combat unit than an employee without CRM skills.</p>
               </div>
             </div>
             <div className={style.SecondBlockConent}>
-              <FontAwesomeIcon icon={faCheck} className={style.SecondBlockIcon}/>
+              <div className={style.SecondBlockIcon}><Image src={checkWhite} alt="check icon"/></div>
               <div>
                 <h3>Further support</h3>
                 <p>The developed CRM systems have the possibility of improvement when new goals and objectives appear. Ensure that all applications and requests (autolids)get into the CRM. Synchronize management and sales department using department reporting. </p>
@@ -139,23 +144,82 @@ export default function Home() {
           <h2 className={style.ThirdBlockHeader}>CAPABILITIES OF CRM SYSTEMS</h2>
           <p className={style.ThirdBlockParagraph}>An effective CRM system helps a business to sell more, takes control of communication with customers, sales and marketing management.</p>
           <div className={style.ThirdBlockCards}>
-            <div className={style.ThirdBlockOneCard1}>
-              <p>Customer base and sales management</p>
+            <div className={style.ThirdBlockOneCard}>
+              <div className={style.ThirdBlockOneCardI}>
+                <Image
+                  src={customerBase}
+                  alt="customer base"
+                />
+              </div>
+              <div className={style.ThirdBlockOneCardRight}>
+                <h3 className={style.ThirdBlockOneCardH3}>Customer base and sales management</h3>
+                <p className={style.ThirdBlockOneCardP}>One of the most important functions of CRM is to save the necessary customer data in an ordered form. For example, contacts, banking details, purchase history.</p>
+              </div>
             </div>
-            <div className={style.ThirdBlockOneCard2}>
-              <p>Customer support and service</p>
+            <div className={style.ThirdBlockOneCard}>
+              <div className={style.ThirdBlockOneCardI}>
+                <Image
+                  src={customerSupport}
+                  alt="customer support"
+                  height={220}
+                />
+              </div>
+              <div className={style.ThirdBlockOneCardRight}>
+                <h3 className={style.ThirdBlockOneCardH3}>Supplier support<br />and customer service</h3>
+                <p className={style.ThirdBlockOneCardP}>All requests from customers fall into the sales funnel of the CRM system. In order to ensure prompt work with clients, in CRM the following tools are implemented.</p>
+              </div>
             </div>
-            <div className={style.ThirdBlockOneCard3}>
-              <p>Employees (tasks, KPI)</p>
+            <div className={style.ThirdBlockOneCard}>
+              <div className={style.ThirdBlockOneCardI}>
+                <Image
+                  src={employeesKPI}
+                  alt="employees KPI"
+                  height={220}
+                />
+              </div>
+              <div className={style.ThirdBlockOneCardRight}>
+                <h3 className={style.ThirdBlockOneCardH3}>Employees, leaderships<br />(tasks, KPI)</h3>
+                <p className={style.ThirdBlockOneCardP}>Make management decisions based on data. In the field of personnel control and management, a modern CRM system based on all platforms allows.</p>
+              </div>
             </div>
-            <div className={style.ThirdBlockOneCard4}>
-              <p className={style.ThirdBlockOneCardP}>Telephony</p>
+            <div className={style.ThirdBlockOneCard}>
+              <div className={style.ThirdBlockOneCardI}>
+                <Image
+                  src={telephony}
+                  alt="telephony CRM"
+                  height={220}
+                />
+              </div>
+              <div className={style.ThirdBlockOneCardRight}>
+                <h3 className={style.ThirdBlockOneCardH3}>Telephony<br />and calltracking</h3>
+                <p className={style.ThirdBlockOneCardP}>With the help of end-to-end multi-channel analytics and call tracking , you will be able to track the client's path from viewing an ad to getting into the sales funnel.</p>
+              </div>
             </div>
-            <div className={style.ThirdBlockOneCard5}>
-              <p className={style.ThirdBlockOneCardP}>Omnichannel</p>
+            <div className={style.ThirdBlockOneCard}>
+              <div className={style.ThirdBlockOneCardI}>
+                <Image
+                  src={systemIntegration}
+                  alt="integration CRM with other systems"
+                  height={180}
+                />
+              </div>
+              <div className={style.ThirdBlockOneCardRight}>
+                <h3 className={style.ThirdBlockOneCardH3}>Integration with other systems and services</h3>
+                <p className={style.ThirdBlockOneCardP}>Integration with other systems and services CRM systems have the ability to integrate with any external programs, services and equipment based on open standards.</p>
+              </div>
             </div>
-            <div className={style.ThirdBlockOneCard6}>
-              <p>Integration with other systems and services</p>
+            <div className={style.ThirdBlockOneCard}>
+              <div className={style.ThirdBlockOneCardI}>
+                <Image
+                  src={processesModeling}
+                  alt="integration CRM with other systems"
+                  height={180}
+                />
+              </div>
+              <div className={style.ThirdBlockOneCardRight}>
+                <h3 className={style.ThirdBlockOneCardH3}>Modeling and automation of processes</h3>
+                <p className={style.ThirdBlockOneCardP}>CRM systems on the 1C platform have a built-in business process editor (BPMN) for automating the organization's processes and regulating key processes.</p>
+              </div>
             </div>
           </div>
         </div>
