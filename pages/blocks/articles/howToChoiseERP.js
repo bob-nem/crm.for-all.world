8511a7e0a986cf8386a86ee5217845312f753ded
Bob <bob.nem@ya.ru>
2021-10-10 22:49:35 +0300

    Articles block in process

---
+++ pages/blocks/articles/howToChoiseERP.js
@@ -0,0 +1,109 @@
+<p>
+ERP - Enterprise Resource Planning or Enterprise Resource Planning. An ERP system is an information system, a program that facilitates the collection, organization, analysis and dissemination of information in a company. 
+
+This is something like an electronic locker in which you put folders. Each folder is a separate software module for some process. For example, CRM, accounting, personnel management. All information is stored and changed only in this cabinet. All folders have locks, and the manager gives keys (access) to those who need to work there. When you buy such a locker, together with the seller, you choose what kind of daddies you need to put there, that is, which modules your company needs. The advantage of such an electronic cabinet is that you can easily take out any module from there or put a new one. And in the version with ERP Odoo, all folders-modules there also integrate themselves with each other. 
+
+Such a system helps to avoid repetitions and loss of important data, significantly reduces the number of errors and makes it very convenient to monitor the business, allows you to upload statistics, clearly see why efficiency decreases, and so on. 
+
+Initially, the ERP system was created for manufacturing companies. That is, it is a medium or large business that has several departments, branches (possibly in different countries), has its own production, and which simply critically needs a good organization of large amounts of data. 
+
+However, now ERP systems have become more flexible and affordable. They are actively used in the service sector, retail, logistics. Today, such systems can be useful even for small businesses, providing space and opportunities for growth. 
+
+
+When you need an ERP system
+
+
+1. There is no resource where you can get full information about the state of affairs in the company.
+
+Will you be able to quickly answer what number of orders you have per day? Or what is your average return on sales? Thanks to the ERP system, management will be able to get a holistic view of what is happening in the company at any time. 
+
+2. You have a lot of disparate programs to work with.
+
+When systems for internal processes (like accounting) and systems for working with customers, suppliers, partners work separately, this can negatively affect the efficiency of your business. The ERP system allows you to accumulate accurate data in real time and store it in one place, which allows employees to quickly make the right decisions and concentrate on performing more important tasks.
+
+3. Your customers suffer from delays and errors.
+
+When sales, inventory, and customer data are stored in different locations, this can cause unpleasant delivery situations: at the wrong time, to the wrong customer, or with an order confirmation for a product that is not in stock. With an ERP system, the customer service manager will always be able to give accurate information about the status of the product, and the system itself will form a purchase request based on pre-configured rules.
+
+4. You have a complex IT infrastructure in your company and it is increasingly difficult for you to maintain it in working order. 
+
+Every time you need to update existing software, implement a new one, or remove something, on the contrary, you face huge costs, it takes you a lot of time and you get a "so-so" result. All this is because there are problems with integration, and separate work is required with each system. When implementing an ERP system, the supplier is responsible for all updates and technical support. In addition, the modular structure of the ERP system allows you to quickly and easily integrate new modules and remove unnecessary ones.
+
+What is important to consider when you decide to implement an ERP system
+
+
+- Decide on the implementation goals, what you expect from automation. Answer the following questions:
+
+1. Why do you need ERP? (for example, you need more gold)
+
+2. How will you do the implementation? (for example, IT pile or outsourcing)
+
+3. When do you want to implement? (for example, today, tomorrow, in a year)
+
+- Make a clear list of requirements before looking for suppliers
+
+- Efficiency and possible payback periods of the information system. An ERP system can open up a huge number of new opportunities for you, but you need to be sure of this. To do this, you should calculate everything before buying.
+
+- Your budget. How much are you willing to spend on implementing an ERP system? Do not be afraid of the cost. For example, when implementing Odoo ERP, you choose only the necessary applications for your business at the moment, the price will be quite lifting. 
+
+- Number of users. Who will have access to the system? The cost may also depend on it.
+
+- A team for the implementation of an ERP system. In addition to specialists from the system supplier, this team should include employees of your company who are well familiar with the specifics of your business processes and will subsequently work with the implemented system. This moment is very important, because without the active help of your specialists, it will not be possible to implement the system qualitatively.
+ 
+- Be ready to give your all at the stage of writing the TOR.
+
+- Employee training. To start using the system effectively, your specialists will first need to go through a series of trainings.
+
+- Get ready for changes. A centralized system will quickly show where your business is sagging and how it can be made more efficient. 
+
+
+- After the implementation, your employees will have a lot of time, so be ready to offer them new tasks :)
+
+
+Key points that will help you find a suitable ERP system
+
+
+1. Type of ERP system.
+
+On-premise - local software that is installed on computers and servers of your company. With such an ERP system, you will get full control over information security, which sometimes turns out to be critical for the company. Since the entire system is located on your territory, the company has full control over its assets and is fully responsible for its safety and integrity.  In addition, there is a need to create a dedicated infrastructure and support IT personnel to ensure the operation of such a system.
+
+Cloud-based is an ERP software that is hosted and managed by your vendor outside of the office in the cloud. With this deployment option, you can focus on managing your business rather than on IT administration. 
+
+The hybrid approach combines the characteristics of a cloud and on-premises solution. It makes it possible to maintain control over the company's data and at the same time add more mobility to work. The hybrid system allows you to place part of the infrastructure on local servers, and part in the cloud. The cloud part is synchronized with the local software. 
+
+2. Choose a system for the specifics of your business. That is, if you are engaged in sales, not production, then you need an ERP solution to automate sales. 
+
+Try to find a supplier who has already worked with companies like yours. And if it doesn't work out that way, then look for someone who will be ready to quickly understand and sort everything out. Whoever implements the system must really understand your business processes.
+
+3. The interface of the ERP system should be convenient and understandable. To do this, there are demo versions of programs that you can test and evaluate whether such a system is comfortable for you.
+
+4. Scalability of the system.  
+
+5. Customization. Will employees be able to customize the system for themselves? And are there any unusual settings that your company may need?
+
+6. Make sure that the supplier will provide support and support for the implementation and subsequent use of the ERP system.
+
+
+
+Total:
+
+1. Decide what you want to get from the ERP system.
+
+2. Calculate the budget.
+
+3. Make a preliminary implementation strategy. 
+
+4. Form a group of persons responsible for the implementation of the system on the part of your company.
+
+5. Try the demo versions of the systems that you liked the most and discuss with colleagues. 
+
+6. Read reviews about the selected company.
+
+7. Carefully study the license agreement. 
+
+8. Make your choice.
+
+
+
+And most importantly, remember: the chosen ERP system should simplify and speed up your work, and not vice versa
+</p>
--- pages/blocks/home/articles.js
+++ pages/blocks/home/articles.js
@@ -1,41 +1,53 @@
 import style from '../../../styles/Articles.module.css'
+import Image from 'next/image'
+
+import ChoiseImg from '../../../images/backgrounds/children-trianing.webp'
+
 
 export default function Articles() {
     return (
         <div className={style.Wrapper} id="articles">
             <h2 className={style.Header}>ARTICLES</h2>
             <div className={style.Container}>
-                <div className={style.Box} style={{backgroundColor: "#E0FC45", border: "2px solid #FFCF46"}}>
+                <div className={style.Box}>
                     <div className={style.Content}>
-                        <h3>How to choise ERP system</h3>
+                        <h3 className={style.H3Hover}>How to choise ERP system</h3>
+                        <div style={{display: "inline-flex"}}>
+                            <p style={{width: "45%", textAlign: "justify", padding: "1em 1em 0 0", alignSelf: "center"}}>You have been quite a technology company for a long time. The documentation has been digitized, all processes are carried out in special information systems for each department. But there is somehow a lot of everything, you do not understand where the money goes, who is to blame for the mistakes and you want some order and clarity. Or you just started a business and immediately want to start all processes on automated rails.</p>
+                            <Image
+                                src={ChoiseImg}
+                                alt={"Childrens choose electronic-document sysem"}
+                            />
+                        </div>
+
                     </div>
                 </div>
-                <div className={style.Box} style={{backgroundColor: "#FF8038", border: "2px solid #32E5B4"}}>
+                <div className={style.Box}>
                     <div className={style.Content}>
                         <h3>Differences between ERP and CRM systems</h3>
                     </div>
                 </div>
-                <div className={style.Box} style={{backgroundColor: "#6BFDB3", border: "2px solid #FF736B"}}>
+                <div className={style.Box}>
                     <div className={style.Content}>
                         <h3>Open source ERP and CRM systems review</h3>
                     </div>
                 </div>
-                <div className={style.Box} style={{backgroundColor: "#1E4EFC", border: "2px solid #FFB100"}}>
+                <div className={style.Box}>
                     <div className={style.Content}>
                         <h3>Advantages of cloud infrastructure</h3>
                     </div>
                 </div>
-                <div className={style.Box} style={{backgroundColor: "#643A0C", border: "2px solid #5D0B1E"}}>
+                <div className={style.Box}>
                     <div className={style.Content}>
                         <h3>Errors in the implementation of automated processes</h3>
                     </div>
                 </div>
-                <div className={style.Box} style={{backgroundColor: "#28590B", border: "2px solid #083242"}}>
+                <div className={style.Box}>
                     <div className={style.Content}>
                         <h3>Synchronization of the SCADA system with the corporate ERP</h3>
                     </div>
                 </div>
-                <div className={style.Box} style={{backgroundColor: "#083242", border: "2px solid #28590B"}}>
+                <div className={style.Box}>
                     <div className={style.Content}>
                         <h3>Power and strength of top paid ERP systems</h3>
                     </div>
