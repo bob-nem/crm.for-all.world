4881800e487ce6857afc057f2a890b7e86308be5
bob <bob.nem@ya.ru>
2021-12-28 15:20:11 +0300

    warehouse article write

--- pages/articles/wms-systems.js
+++ pages/articles/wms-systems.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import style from '../../styles/articles/OpenSourceSystems.module.css'
+import style from '../../styles/articles/Warehouse.module.css'
 import Footer from './footer'
 
 import TopImg from '../../images/articles/erp-and-crm-differences_topBG.webp'
@@ -27,48 +27,124 @@ export default function WmsSystems() {
                 <h2>Pros and cons of WMS</h2>
                 <p>Sooner or later there comes a time when traditional methods of warehouse accounting cease to meet the increased needs of business. Due to the increasing turnover of goods, warehouse processes lose manageability, and the organization of affairs in the warehouse increasingly resembles chaos. In this case, a large-scale reorganization cannot be avoided, and the question arises about the automation of warehouse activities. According to the developers of WMS, among the advantages that the company that has implemented this system receives are the most efficient use of storage facilities, ensuring an optimal level of inventory, uninterrupted shipment of goods, etc.</p>
                 <p>In practice, however, things may be different. Most automated systems of the ERP class, which includes WMS, as a rule, consist of three components. The first of them is the interface through which users interact with the system. The second is a data warehouse that provides access to information. The third is business logic, that is, special procedures through which data processing algorithms are implemented in response to a user request.</p>
-                <p>The introduction of ERP systems belonging to the class of "heavy" and practically not offered in the form of boxed solutions is associated with high costs and can last for a long time. Most companies come to the need to have a WMS when the volume of warehouse activity is large and significant changes are required for reorganization. Management should understand that it is possible to increase the efficiency of the warehouse by implementing WMS only over time. The problems associated with the transition to a new method of work are also inevitable.</p>
+                <p>The introduction of ERP systems belonging to the class of &quot;heavy&quot; and practically not offered in the form of boxed solutions is associated with high costs and can last for a long time. Most companies come to the need to have a WMS when the volume of warehouse activity is large and significant changes are required for reorganization. Management should understand that it is possible to increase the efficiency of the warehouse by implementing WMS only over time. The problems associated with the transition to a new method of work are also inevitable.</p>
                 <p>Firstly, the staff will have to learn a lot and get used to working in a new way. WMS will require drastic changes in all work processes, which will be a stressful situation for most. You need to be prepared for the fact that not everyone will agree to accept such radical changes and some employees will have to part ways. Some of the staff may openly or covertly sabotage the changes, which also will not speed up the transition process. Staff training always requires a lot of effort, time and money, and if you try to save money on it, it will probably do more harm than good.</p>
                 <p>Secondly, the external environment is also unlikely to be ready for significant changes. Any large trading company has a large number of suppliers, each of which is used to working in its own way, and WMS imposes strict requirements for the acceptance of goods. For example, a large number of unmarked goods in the flow of goods will cause problems in the operation of the warehouse and greatly slow down its functioning. Therefore, it is necessary to build relationships with suppliers in advance, developing uniform rules for the shipment of goods.</p>
-                <p>Thirdly, it is necessary to take into account the scale of the company's growth and the need for further expansion. The main disadvantage of WMS is the low warehouse fill factor, usually not exceeding 0.5. This factor should be kept in mind when designing a warehouse and building long-term forecasts for the development of the enterprise.</p>
+                <p>Thirdly, it is necessary to take into account the scale of the company&apos;s growth and the need for further expansion. The main disadvantage of WMS is the low warehouse fill factor, usually not exceeding 0.5. This factor should be kept in mind when designing a warehouse and building long-term forecasts for the development of the enterprise.</p>
                 <p>Finally, it is necessary to keep in mind the difficulties of the transition period. No company can afford to stop the operation of the warehouse for the period of implementation of the automated system. We must be prepared for the fact that for some time the work of the warehouse will be irregular, there will be inconsistencies and errors. In addition, the WMS must collect initial information about the goods and their storage locations. Only as the data accumulates will it be possible to use optimization algorithms to improve the efficiency and speed of the warehouse. At this stage, well-established communications within the company are extremely important: only a good mutual agreement will allow you to resolve misunderstandings as quickly and clearly as possible.</p>
-                <p>In general, the success of the implementation of any "intelligent" system depends, first of all, on how fully and thoroughly the business logic is implemented in it. That is why the process of preparing technical specifications for a WMS system is complicated. At the preliminary stage, it is necessary to carry out a lot of analytical work, prescribe all the actions performed in the warehouse, create ready-made solutions and algorithms. If all automated processes are well structured and formalized during the preparation of the terms of reference, then the system will really allow you to manage the warehouse.</p>
-                <table cellspacing="0" cellpadding="0" style={{width: "90%"}} className="table2008">
+                <p>In general, the success of the implementation of any &quot;intelligent&quot; system depends, first of all, on how fully and thoroughly the business logic is implemented in it. That is why the process of preparing technical specifications for a WMS system is complicated. At the preliminary stage, it is necessary to carry out a lot of analytical work, prescribe all the actions performed in the warehouse, create ready-made solutions and algorithms. If all automated processes are well structured and formalized during the preparation of the terms of reference, then the system will really allow you to manage the warehouse.</p>
+                <h3>The most significant criteria for choosing a warehouse management system from the perspective of potential customers*</h3>
+                <table cellSpacing="0" cellPadding="3em" style={{ width: "95%", margin: "0 auto", marginTop: "1rem" }}>
                     <tbody>
-                        <tr class="thead">
-                            <td bgcolor="#9fadea" align="center"><strong>Критерий</strong></td>
-                            <td bgcolor="#9fadea" align="center"><strong>Степень значимости (процент от общего числа опрошенных)</strong></td>
+                        <tr>
+                            <td bgcolor="#1e6ead" align="center"><strong style={{ color: "white" }}>Criterion</strong></td>
+                            <td bgcolor="#1e6ead" align="center"><strong style={{ color: "white" }}>Degree of importance (percentage of total number of respondents)</strong></td>
                         </tr>
-                        <tr class="odd">
-                            <td> Стоимость решения</td>
+                        <tr>
+                            <td>Cost</td>
                             <td align="center">25%</td>
                         </tr>
-                        <tr class="even">
-                            <td>История продаж решения в России</td>
+                        <tr>
+                            <td>Solution selling history in country</td>
                             <td align="center">31%</td>
                         </tr>
-                        <tr class="odd">
-                            <td>История продаж решения за рубежом</td>
+                        <tr>
+                            <td>Solution selling history in all world</td>
                             <td align="center">3%</td>
                         </tr>
-                        <tr class="even">
-                            <td>Широта функционала</td>
+                        <tr>
+                            <td>Functional latitude</td>
                             <td align="center">58%</td>
                         </tr>
-                        <tr class="odd">
-                            <td>Адаптируемость</td>
+                        <tr>
+                            <td>Adaptability</td>
                             <td align="center">42%</td>
                         </tr>
-                        <tr class="even">
-                            <td>Соответствие решения условиям и бизнес-процессам</td>
+                        <tr>
+                            <td>Compliance with the solutions and business processes</td>
                             <td align="center">81%</td>
                         </tr>
-                        <tr class="odd">
-                            <td>Легкость обучения персонала</td>
+                        <tr>
+                            <td>Ease of staff training</td>
                             <td align="center">60%</td>
                         </tr>
                     </tbody>
                 </table>
+                <p style={{ width: "90%", fontSize: ".8em", margin: "0 auto" }}>* A total of 48 companies potentially interested in warehouse automation were interviewed. Survey participants could choose up to three criteria as the most significant, so the total amount is more than 100%. The results of the received figures were rounded to the whole.</p>
+                <p>If during the preparation of the terms of reference all automated processes are well structured and formalized, then the resulting system will really allow you to manage the warehouse.</p>
+                <h3>Leaders of WMS systems</h3>
+                <p>Today, there are many different WMS systems on the market, which differ from each other, first of all, by the scale of the processed data. To organize the efficient operation of the warehouse, it is necessary to choose a WMS that meets all the requirements of the organization of business processes in the warehouse: working with barcodes, reading expiration dates, GTD numbers, etc. Simultaneously with the introduction of WMS, it will be necessary to purchase additional equipment (special warehouse equipment, racks, terminals), mount a network and install servers. It is important to take into account not only the difference in prices from different suppliers, but also the technical characteristics of the selected equipment, its capabilities and operability, as well as the availability of service centers.</p>
+                <table cellSpacing="0" cellPadding="3em" style={{width: "95%", margin: "0 auto", marginTop: "1rem"}}>
+                    <tbody>
+                        <tr>
+                            <td bgcolor="#1e6ead" align="center" style={{ color: "white" }}><strong>№</strong></td>
+                            <td bgcolor="#1e6ead" align="center" style={{ color: "white" }}><strong>Product name</strong></td>
+                            <td bgcolor="#1e6ead" align="center" style={{ color: "white" }}><strong>Services served for</strong></td>
+                            <td bgcolor="#1e6ead" align="center" style={{ color: "white" }}><strong>Top/ Best Feature</strong></td>
+                        </tr>
+                        <tr>
+                            <td align="center">1</td>
+                            <td><Link href="https://www.netsuite.com/portal/assets/pdf/ds-netsuite-wms.pdf"><a target="_blank">NetSuite WMS</a></Link></td>
+                            <td>Advertising, Apparel, Footwear, Food &#38; Beverage, Healthcare, Health &#38; Beauty Manufacturing etc.</td>
+                            <td>Strategy definition for put away and picking up.</td>
+                        </tr>
+                        <tr>
+                            <td align="center">2</td>
+                            <td><Link href="https://www.fishbowlinventory.com/"><a target="_blank">Fishbowl Inventory</a></Link></td>
+                            <td>Wholesale and Manufacturing.</td>
+                            <td>Provides many plugins. Multi-currency conversions.</td>
+                        </tr>
+                        <tr>
+                            <td align="center">3</td>
+                            <td><Link href="https://3plcentral.com/products/3pl-warehouse-manager/"><a target="_blank">3PL warehouse manager</a></Link></td>
+                            <td>3PL, e-commerce, Retail, Manufacturing.</td>
+                            <td>Centralized management for multiple warehouses and customer data.</td>
+                        </tr>
+                        <tr>
+                            <td align="center">4</td>
+                            <td><Link href="https://www.softeon.com/our-solutions/product-solutions/warehouse-management"><a target="_blank">Softeon</a></Link></td>
+                            <td>Retail, Beverage, Manufacturing, Technology &#38; Electronics, 3PL, Consumer Packaged Goods.</td>
+                            <td>Seamless integration to material handling automation systems.</td>
+                        </tr>
+                        <tr>
+                            <td align="center">5</td>
+                            <td><Link href="https://www.infor.com/solutions/scm/"><a target="_blank">Infor SCM</a></Link></td>
+                            <td>Automotive, Chemical, Aerospace &#38; Defense, Fashion, Oil and gas, Health Care, Electronics, Food &#38; Beverage and many more.</td>
+                            <td>Supports early payment. 3D Visualization.</td>
+                        </tr>
+                        <tr>
+                            <td align="center">6</td>
+                            <td><Link href="https://www.highjump.com/supply-chain-management-solutions/warehouse-management-solutions-wms/warehouse-management-distributors"><a target="_blank">HighJump</a></Link></td>
+                            <td>Automotive &#38; Aerospace, Consumer Packaged Goods Health &#38; Wellness, Food distribution &#38; Processing Beverage, 3PL, Retail, Apparel &#38; Footwear.</td>
+                            <td>Order picking functionality &#38; ERP Integrations.</td>
+                        </tr>
+                        <tr>
+                            <td align="center">7</td>
+                            <td><Link href="https://www.manh.com/products/warehouse-management"><a target="_blank">Manhattan Associates</a></Link></td>
+                            <td>Retail, Consumer Goods, Food Distribution, Apparel and Footwear, Life Science, High-Tech Electronics, 3PL.</td>
+                            <td>Provides more Security options.</td>
+                        </tr>
+                        <tr>
+                            <td align="center">8</td>
+                            <td><Link href="https://www.tecsys.com/supply-chain/solutions/warehouse-management-system/"><a target="_blank">TECSYS WMS</a></Link></td>
+                            <td>Electrical products, Health care, 3PL, Industrial distribution and Service parts.</td>
+                            <td>Customizable workflow, Dynamic slotting.</td>
+                        </tr>
+                        <tr>
+                            <td align="center">9</td>
+                            <td><Link href="https://deposco.com/"><a target="_blank">Bright Warehouse</a></Link></td>
+                            <td>---</td>
+                            <td>Quality Control.</td>
+                        </tr>
+                        <tr>
+                            <td align="center">10</td>
+                            <td><Link href="https://www.consafelogistics.com/solutions/wms/"><a target="_blank">Astro WMS</a></Link></td>
+                            <td>All Industries</td>
+                            <td>Supports integration with ERP systems.</td>
+                        </tr>                       
+                    </tbody>
+                </table>
+                <p>IT professionals specializing in warehouse activities assure that a simple investment in automation will not be able to seriously increase the productivity of the warehouse. A banal replacement of some resources with others, albeit more technologically advanced, is not enough. The competent organization of the exploitation of these resources becomes a non-trivial task: anyway, in any system there is a person on whom a lot depends. Attempts to negate the influence of the human factor by means of automation, of course, are doomed to failure. Introducing even the most modern and technologically advanced management tools, we must not forget that the key to the success of any activity is an organized team of qualified specialists who responsibly approach their work and believe in a common goal.</p>
             </div>
             <Footer />
         </>
