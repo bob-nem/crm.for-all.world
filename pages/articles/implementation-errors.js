8fdce212ce99a9997ebf17127d6f4e19222ce4be
bob <bob.nem@ya.ru>
2021-12-31 17:17:38 +0300

    errors  article write

--- pages/articles/implementation-errors.js
+++ pages/articles/implementation-errors.js
@@ -1,7 +1,67 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import style from '../../styles/articles/Warehouse.module.css'
+import Footer from './footer'
+
+import TopImg from '../../images/articles/erp-and-crm-differences_topBG.webp'
+
 export default function ImplementationErrors () {
     return (
         <div>
-            <p>test</p>
+            <div className={style.Cover}>
+                <div style={{ display: "flex", maxWidth: "60rem", alignItems: "center", margin: "0 auto" }}>
+                    <h1 style={{ fontFamily: "Righteous", textTransform: "uppercase", color: "white", paddingRight: "2em" }}>Top 5 Business Optimization Mistakes</h1>
+                    <div>
+                        <Image
+                            src={TopImg}
+                            alt="study open source corporate systems"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className={style.Content}>
+                <p>Any business owner is interested in its growth and development. How to increase sales? How to reduce costs? What kind of change will result in profit?</p>
+                <p>If you are not only asking these questions, but also making efforts to solve them, then you are optimizing your business.</p>
+                <p>At the same time, optimization can be aimed both at increasing the profit of the enterprise and at increasing the comfort of business management.</p>
+                <p>During the time spent in the field of optimization of the economy of small companies, we have identified the most common mistakes. Studying the article will help you avoid them, saving your money, time and energy.</p>
+                <h2>Mistake #1. Weak work with pricing</h2>
+                <p>Low prices for your goods and services with comparable quality - that&apos;s what can help you take the customer away from competitors, even if everything else in your business is done poorly. However, low prices bring less profit. So what about the prices? Change them every day, trying to find the most profitable combination, or install them once and keep them unchanged?</p>
+                <p>We have encountered both options in working with our clients business processes. The second case turned out to be more common. It is dangerous because due to the inflexibility of the pricing policy, you can quickly lose the existing customer base. Moreover, price is the main competitive tool. By creating certain generosity promotions, you can attract new customers to yourself.</p>
+                <p>So which strategy works best? If your company does not have special installations, then the best solution is to periodically change the prices of your goods and services. How and how often? Seasonally adjusted. At the same time, the changes should not be in the spirit of plus or minus 5%.</p>
+                <p>It is necessary to identify the positions that bring customers to you, a kind of flagships of your sales. Their prices should be as competitive as possible. Perhaps even below market prices. Your offer must be really profitable so that customers come to you again and again.</p>
+                <p><b>And then how to make a profit?</b> Select a group of products or services that go next to the flagships. They should complement the use of inexpensive items. For example, people come to a restaurant choosing inexpensive main dishes, after which they regularly overpay for alcohol, desserts and drinks. It is necessary to break the barrier of distrust of the client, making him loyal to his services.</p>
+                <p>Try for the client and then there is a high chance that he will show loyalty to you and overpay for related services. You have to understand: you should absolutely not care what goods come to you from the sale of the goods. The main task is to maximize it and customer loyalty to your company.</p>
+                <p>Therefore, <b>change prices regularly</b>, increase them and decrease them at the same time, but do it wisely. Next, we will tell you about the tools that will help you do this with minimal effort.</p>
+                <h2>Mistake #2. Work in the wrong direction</h2>
+                <p>You can often meet young people who have decided to go into business. They can be praised for this. Indeed, many of them put a huge amount of effort into making their business flourish. However, most often the business, so to speak, does not go.</p>
+                <p>Why is this happening? A young team is working, creating new ideas, and there are quite a few real sales?</p>
+                <p>Let&apos;s look at this situation with an example. Imagine an ordinary beauty salon that could well be located in your area. The target audience is women, most of whom are housewives.</p>
+                <p>The main problem of this business is the inability to serve all customers after 5 to 8 pm, while there are almost no customers from morning to evening. How to optimize this situation?</p>
+                <p>Maybe we can change the prices, make a promotional offer? Or will we launch targeted advertising? It seems that having done all this, the business should go. But all these actions would not solve the main problem, because of which women do not go to the salon at this time. Most often, they simply have no one to leave their child with.</p>
+                <p>And now, in many enterprises, cafes, beauty salons, we are already more often meeting children&apos;s corners where you can leave children, entrusting their safety to specialists.</p>
+                <p>Such a solution, which does not lie at all in the plane of typical optimization tools, really works. How to think of such a thing? Again and again ask yourself the question of why a client does not come to you. Look for the cause and eliminate it.</p>
+                <h2>Mistake #3. Lack of a systematic approach</h2>
+                <p>When doing optimization, you need to remember that any change in your business will affect all its components in one way or another. For example, you can&apos;t just mindlessly reduce the number of staff in an organization. This problem is especially common in the work of enterprises.</p>
+                <p>It is not always possible for the head of an organization to assess the real usefulness of a particular specialist from a height. Mindlessly pulling parts out of the car, you will not make it better. It will be more useful to determine how best to use the work of the team so that it not only does not sit idle, but also does not engage in useless tasks.</p>
+                <p>Or another situation. You can come up with a new company development plan, think over a new pricing policy, determine the optimal volume of production of goods or services, but forget one very important thing. Any plan needs to be monitored in execution.</p>
+                <p><strong>The quality of planning directly depends on the quality of its execution.</strong> Moreover, these two processes are completely different. But the issue of control can be simplified. Create a plan, consulting with the performers, highlight the exact criteria of success, which you can then focus on. Lay down reserves, both monetary and material. Be prepared for drastic changes if a force majeure situation occurs.</p>
+                <p>In general, keep everything in mind and know that if the problem repeats again and again, then it is natural. And natural problems need to be solved, they will not end by themselves.</p>
+                <h2>Mistake #4. Using only intuition or the absence of an economic model</h2>
+                <p>In the last problem, we advised to keep everything in mind. The advice is good, but difficult to implement. Of course, it is possible and necessary to simplify this thought process.</p>
+                <p>There are many ways to transfer all your experience and business sense into an economic model. For example, it will be useful for any company to collect statistics on sales, as well as keep records of company expenses. You can do this in a school notebook, but there are better options.</p>
+                <p>Most often we find economic models created by managers or economists based on Excel. The advantages of such models are the presence of internal prescribed calculations. Indeed, what could be better than inserting your new company data and getting a ready-made report on what needs to be done tomorrow?</p>
+                <p>However, the quality of such models directly depends on the user who composes it. If he makes one mistake in the formulas, makes an inconvenient design or misses taking into account some important factor — that&apos;s it, such a model does not work for the benefit of the company. High quality models, as a rule, can be found only in large companies. And what remains for small and medium-sized businesses?</p>
+                <p>Special software aimed at solving specific tasks works best. If you need to keep accounting records, 1C is better, the accounting Contour has not yet been created. Are you interested in services for processing incoming customer requests? There are many CRM systems that will help you solve this problem.</p>
+                <p>And if you need to use your data to get ready-made solutions to increase your company&apos;s profits, then SimplexPlan software will help you. This is a comprehensive program that creates an economic model of your organization, analyzes it itself and generates ready-made text reports. By and large, it completely closes the issue of conducting economic planning in companies.</p>
+                <p>Therefore, <b>you do not need to keep everything in mind</b>, believe me, you will already have something to think about while solving operational tasks. Use ready-made services. They can be very cheap and at the same time solve real tasks that would take you a lot of time and resources.</p>
+                <h2>Mistake #5. High optimization price</h2>
+                <p>As part of our work, we have come across the opinion that optimization is something that only large companies can afford. It is possible to understand this opinion, because search queries about financial and production consulting often show large companies whose price tags for services can reach several million, and the project completion period is several months.</p>
+                <p>These solutions, of course, are not suitable for small businesses. That&apos;s why we work in this, unoccupied service sector. We help to optimize the business of small companies and teach this to everyone.</p>
+                <p>As you can see, a lot can be achieved through the use of good economic models and creative systems thinking. Don&apos;t believe those companies who say that optimization is difficult and expensive. No, on the contrary. The costs for it should be minimal so that the benefits are guaranteed.</p>
+                <p>Do not repeat the mistakes described in this article, optimize your business, and then <strong>get a decent income and pleasure from work</strong>.</p>
+                <p>Subscribe, then we will talk about specific ways to optimize the business of different industries. Leave comments so that we can understand which industry is more relevant to you. Let&apos;s start with her!</p>
+            </div>
+            <Footer />
         </div>
     )
 }
